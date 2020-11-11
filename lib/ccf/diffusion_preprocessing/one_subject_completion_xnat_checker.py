920fcb5c37f5f5ba62cf73d88d447f783dc350d0
Mike Hodge <hodgem@login02.cluster>
2020-11-10 21:55:00 -0600

    For diffusion pipeline check /pipeline_tools/pipelines (hcp-pipelines)

--- lib/ccf/diffusion_preprocessing/one_subject_completion_xnat_checker.py
+++ lib/ccf/diffusion_preprocessing/one_subject_completion_xnat_checker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # import of built-in modules
+import os
 import sys
 
 # import of third-party modules
@@ -11,6 +12,8 @@ import ccf.diffusion_preprocessing.one_subject_job_submitter as one_subject_job_
 import ccf.one_subject_completion_xnat_checker as one_subject_completion_xnat_checker
 import ccf.subject as ccf_subject
 import utils.my_argparse as my_argparse
+import utils.file_utils as file_utils
+import utils.os_utils as os_utils
 
 class OneSubjectCompletionXnatChecker(one_subject_completion_xnat_checker.OneSubjectCompletionXnatChecker):
 
@@ -32,6 +35,30 @@ class OneSubjectCompletionXnatChecker(one_subject_completion_xnat_checker.OneSub
 		dirs = []
 		dirs.append(archive.structural_preproc_dir_full_path(subject_info))
 		return dirs
+
+
+	def list_of_expected_files(self, working_dir, fieldmap, subject_info):
+
+		ptFilePath = os.sep + 'pipeline_tools' + os.sep + 'pipelines' + os.sep + 'expected_files' + os.sep + 'DiffusionPreprocessing.txt'
+		print(ptFilePath)
+		hcp_run_utils = os_utils.getenv_required('HCP_RUN_UTILS')
+		if os.path.isfile(ptFilePath):
+			f = open(ptFilePath)
+		elif os.path.isfile(hcp_run_utils + os.sep + self.processing_name + os.sep
+				 + self.expected_output_files_template_filename(fieldmap)):
+			f = open(hcp_run_utils + os.sep + self.processing_name + os.sep
+					 + self.expected_output_files_template_filename(fieldmap))
+		else:
+			xnat_pbs_jobs = os_utils.getenv_required('XNAT_PBS_JOBS')
+			f = open(xnat_pbs_jobs + os.sep + self.processing_name + os.sep
+					 + self.expected_output_files_template_filename(fieldmap))
+			
+		root_dir = os.sep.join([working_dir, subject_info.subject_id + '_' + subject_info.classifier])
+		l = file_utils.build_filename_list_from_file(f, root_dir,
+			 subjectid=subject_info.subject_id + '_' + subject_info.classifier,
+			 scan=subject_info.extra)
+		return l
+
 	
 if __name__ == "__main__":
 
